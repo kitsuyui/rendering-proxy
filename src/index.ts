c11752f9f46e985ed2022bc8a985360d1b2641f3
kitsuyui <kitsuyui@kitsuyui.com>
2022-08-04 05:46:51 +0900

    Implement command-line interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+export * as cli from './cli';
 export * as server from './server';
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,40 @@
-import { server } from './';
+#!/usr/bin/env node
+import { server, cli } from './';
+import yargs from 'yargs';
 
-server.main();
+export async function main(): Promise<void> {
+  yargs
+    .command(
+      'cli [url]',
+      'CLi mode',
+      (builder) => {
+        return builder.positional('url', {
+          type: 'string',
+          demandOption: true,
+        });
+      },
+      async (args) => {
+        await cli.main(args.url);
+      }
+    )
+    .command(
+      'server',
+      'Server mode',
+      (builder) => {
+        return builder.option('p', {
+          alias: 'port',
+          type: 'number',
+          default: 8080,
+        });
+      },
+      async (args) => {
+        await server.main({ port: args.p });
+      }
+    )
+    .demandCommand(1)
+    .help().argv;
+}
+
+if (require.main === module) {
+  main();
+}
