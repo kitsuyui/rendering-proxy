ccb4f3be563da895395c7e9d68c4f4929a0ada09
kitsuyui <kitsuyui@kitsuyui.com>
2022-08-09 07:43:59 +0900

    Add test for command-line argument parser

--- src/main.ts
+++ src/main.ts
@@ -6,56 +6,73 @@ import { LifecycleEvent, lifeCycleEvents } from './render';
 
 import { cli, server } from './';
 
-export async function main(): Promise<void> {
-  yargs
-    .command(
-      'cli [url]',
-      'CLi mode',
-      (builder) => {
-        return builder
-          .option('u', {
-            alias: 'waitUntil',
-            default: 'networkidle',
-            choices: lifeCycleEvents,
-          })
-          .option('b', {
-            alias: 'browser',
-            default: 'chromium',
-            choices: selectableBrowsers,
-          })
-          .positional('url', { type: 'string', demandOption: true });
-      },
-      async (args) => {
-        const url = args.url;
-        const waitUntil = args.u as LifecycleEvent;
-        const browser = args.b as SelectableBrowsers;
-        await cli.main({ url, waitUntil, name: browser });
-      }
-    )
-    .command(
-      'server',
-      'Server mode',
-      (builder) => {
-        return builder
-          .option('p', {
-            alias: 'port',
-            type: 'number',
-            default: 8080,
-          })
-          .option('b', {
-            alias: 'browser',
-            default: 'chromium',
-            choices: selectableBrowsers,
-          });
-      },
-      async (args) => {
-        const browser = args.b as SelectableBrowsers;
-        const port = args.p as number;
-        await server.main({ port, name: browser });
-      }
-    )
+type ParsedArgs =
+  | {
+      [x: string]: unknown;
+      u: string;
+      url: string;
+      p: number;
+      b: string;
+      _: (string | number)[];
+      $0: string;
+    }
+  | {
+      [x: string]: unknown;
+      u: string;
+      url: string;
+      p: number;
+      b: string;
+      _: (string | number)[];
+      $0: string;
+    };
+
+export async function parseArgs(args: string[]): Promise<ParsedArgs> {
+  return await yargs(args)
+    .command('cli [url]', 'CLi mode', (builder) => {
+      return builder
+        .option('u', {
+          alias: 'waitUntil',
+          default: 'networkidle',
+          choices: lifeCycleEvents,
+        })
+        .option('b', {
+          alias: 'browser',
+          default: 'chromium',
+          choices: selectableBrowsers,
+        })
+        .positional('url', { type: 'string', demandOption: true });
+    })
+    .command('server', 'Server mode', (builder) => {
+      return builder
+        .option('p', {
+          alias: 'port',
+          type: 'number',
+          default: 8080,
+        })
+        .option('b', {
+          alias: 'browser',
+          default: 'chromium',
+          choices: selectableBrowsers,
+        });
+    })
     .demandCommand(1)
     .help().argv;
+}
+
+export async function main(): Promise<void> {
+  const argv = await parseArgs(process.argv.slice(2));
+  if (argv._[0] === 'cli') {
+    await cli.main({
+      url: argv.url,
+      waitUntil: argv.u as LifecycleEvent,
+      name: argv.b as SelectableBrowsers,
+    });
+  } else if (argv._[0] === 'server') {
+    await server.main({
+      port: argv.p,
+      name: argv.b as SelectableBrowsers,
+    });
+  }
 }
 
 if (require.main === module) {
