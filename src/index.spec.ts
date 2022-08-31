cd301aca2a932f7a047f332ef94c8b095f84696c
kitsuyui <kitsuyui@kitsuyui.com>
2022-08-31 15:07:35 +0900

    Add test for imports

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+import * as RenderingProxy from './';
+
+describe('RenderingProxy', () => {
+  test('test imports', () => {
+    expect(RenderingProxy).toBeDefined();
+    expect(RenderingProxy.cli).toBeDefined();
+    expect(RenderingProxy.cli.main).toBeDefined();
+    expect(RenderingProxy.server).toBeDefined();
+    expect(RenderingProxy.server.main).toBeDefined();
+  });
+});
