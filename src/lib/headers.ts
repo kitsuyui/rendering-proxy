ec4581e1dd4a3c1c4cfec3f1f129926a5b913429
kitsuyui <kitsuyui@kitsuyui.com>
2023-08-05 22:10:02 +0900

    Add comment about hop-by-hop headers and content dependent headers
    - daily refactoring

--- src/lib/headers.ts
+++ src/lib/headers.ts
@@ -16,6 +16,18 @@ type Headers = {
   [key: string]: string;
 };
 
+/**
+ * Note: Hop-by-hop headers are meaningful only for a single transport-level connection, and are not stored by caches or forwarded by proxies.
+ * See also: https://developer.mozilla.org/en-US/docs/Web/HTTP/Compression#hop-by-hop_compression
+ * So, we should exclude them from the response headers before passing them to the client.
+ * And we should exclude content-dependent headers as well.
+ */
+
+/**
+ * Exclude hop-by-hop headers from the response headers.
+ * @param headers {Headers}
+ * @returns headers {Headers}
+ */
 export function excludeHopByHopHeaders(headers: Headers): Headers {
   const result: Headers = {};
   for (const key in headers) {
@@ -27,6 +39,11 @@ export function excludeHopByHopHeaders(headers: Headers): Headers {
   return result;
 }
 
+/**
+ * Exclude content-dependent headers from the response headers.
+ * @param headers {Headers}
+ * @returns headers {Headers}
+ */
 export function excludeContentDependentHeaders(headers: Headers): Headers {
   const result: Headers = {};
   for (const key in headers) {
@@ -38,6 +55,11 @@ export function excludeContentDependentHeaders(headers: Headers): Headers {
   return result;
 }
 
+/**
+ * Exclude unused headers from the response headers. (Hop-by-hop headers and content-dependent headers)
+ * @param headers {Headers}
+ * @returns headers {Headers}
+ */
 export function excludeUnusedHeaders(headers: Headers): Headers {
   return {
     ...excludeContentDependentHeaders({
